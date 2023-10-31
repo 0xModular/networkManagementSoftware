416d3271fcd6ff1ba678d1a7f70b415a34f91b00
Ubljudok <Ubljudok1@gmail.com>
2023-10-31 18:41:15 -0400

    Added Toolbar

--- src/gui/MainFrame.h
+++ src/gui/MainFrame.h
@@ -18,8 +18,27 @@ class MainFrame : public wxFrame {
 
 	private:
 
-		wxMenuBar* mm = new wxMenuBar(); //Main Menu
+		//Main Menu
+		wxMenuBar* mm = new wxMenuBar();
+		
+		//--MAIN MENU IDS--//
+		inline static const int ID_CONFIG = 1;
+		inline static const int ID_MANAGEDEVICES = 2;
+		inline static const int ID_VIEWNETWORK = 3;
+		inline static const int ID_MANAGEUSERS = 4;
+		inline static const int ID_UPDATEUSER = 5;
+		inline static const int ID_OPENDOCS = 6;
 
+
+
+		//Toolbar
+		wxToolBar* tb = new wxToolBar(this, wxID_ANY, wxDefaultPosition, wxDefaultSize, wxTB_HORIZONTAL);	
+
+		//--TOOLBAR IDS--//
+		inline static const int ID_SELECT = 7;
+		inline static const int ID_ADDDEVICE = 8;
+		inline static const int ID_TESTCONNECTION = 9;
+		inline static const int ID_NOTE = 10;
 };
 
 
@@ -62,11 +81,24 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 	//--NETWORK--//
 	auto mm_Network = new wxMenu();
 
-	//--USERS--// - Restricted to Network Admin
+	auto mm_NetworkConfigure = mm_Network->Append(ID_CONFIG,"Configure Network"); //Configure Network - Restricted to Network Admin
+
+	auto mm_NetworkManageDevices = mm_Network->Append(ID_MANAGEDEVICES, "Manage Devices"); //Manage Devices - Restricted to Network Admin
+
+	auto mm_NetworkViewInfo = mm_Network->Append(ID_VIEWNETWORK, "View Network Information"); //View Network Information
+
+
+	//--USERS--//
 	auto mm_Users = new wxMenu();
+
+	auto mm_UsersManage = mm_Users->Append(ID_MANAGEUSERS, "Manage Users"); //Manage Users - Restricted to Network Admin
+
+	auto mm_UsersUpdate = mm_Users->Append(ID_UPDATEUSER, "Update Account Information"); //Update User Account Information
 
 	//--HELP--//
 	auto mm_Help = new wxMenu();
+
+	auto mm_HelpOpenDocs = mm_Help->Append(ID_OPENDOCS, "Open Documentation"); //Link to Documentation
 	
 	//--APPLY MAIN MENU--//	
 	mm->Append(mm_File, "&File");
@@ -77,6 +109,24 @@ MainFrame::MainFrame(const wxString& title) : wxFrame(nullptr, wxID_ANY, title) 
 
 	SetMenuBar(mm);
 
+
+
+	//Toolbar
+
+	//--ADD TOOLBAR OPTIONS--//
+	tb->AddTool(ID_SELECT, "Select", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR)); //Cursor is in Select Mode
+	tb->AddTool(ID_ADDDEVICE, "Add Device", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR)); //Cursor will Add Device when clicking on Network Field - Restricted to Network Admin
+	tb->AddTool(ID_TESTCONNECTION, "Test Connection", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR)); //Cursor will Select Two Devices to Test
+	tb->AddTool(ID_NOTE, "Place Note", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR)); //Cursor will Place Note when clicking on Network Field
+	tb->AddSeparator();
+	tb->AddTool(wxID_ZOOM_IN, "&Zoom In", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR)); //Network Field will be Zoomed In
+	tb->AddTool(wxID_ZOOM_OUT, "&Zoom Out", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR)); //Network Field will be Zoomed Out
+	tb->AddTool(wxID_ZOOM_FIT, "&Reset Zoom", wxArtProvider::GetBitmap(wxART_COPY, wxART_TOOLBAR)); //Network Field Zoom will be set to Default
+	
+	//--APPLY TOOLBAR--//
+	SetToolBar(tb);
+	tb->Realize();
+	
 
 
 	//Set Minimum Window Size
