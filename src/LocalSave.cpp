cbebe1df156eb5794daa46ab3a134f80d83d152b
LayneUhhhhh <lhb1001@sru.edu>
2023-11-15 01:34:39 +0000

    hhh

--- src/LocalSave.cpp
+++ src/LocalSave.cpp
@@ -9,16 +9,6 @@
 
 bool LocalSave::openFile(std::string inputFileString, LocalSave *createdSave){
 
-<<<<<<< HEAD
-	// Open the source file for reading
-	std::ifstream sourceFile(this->file, std::ios::binary);
-    
-    	if (!sourceFile) {
-        
-		return false;
-    
-	}
-=======
     std::ifstream *temp = new std::ifstream(inputFileString, std::ios::binary);
 
     if (!temp) 
@@ -39,7 +29,6 @@ LocalSave::LocalSave(std::ifstream *input){
 
 
 bool LocalSave::BackupSave(std::string destinationPath) {
->>>>>>> 0df8151 (hh)
 
     	// Open the destination file for writing
     	std::ofstream destinationFile(destinationPath, std::ios::binary);
@@ -50,13 +39,8 @@ bool LocalSave::BackupSave(std::string destinationPath) {
     
 	}
 
-<<<<<<< HEAD
-    	// Copy the content of the source file to the destination file
-    	destinationFile << sourceFile.rdbuf();
-=======
     // Copy the content of the source file to the destination file
     destinationFile << f->rdbuf();
->>>>>>> 0df8151 (hh)
 
     	// Check if the copy was successful
     
@@ -66,16 +50,8 @@ bool LocalSave::BackupSave(std::string destinationPath) {
     
 	}
 
-<<<<<<< HEAD
-    	// Close the files
-	sourceFile.close();
-    	destinationFile.close();
-
-    	return true;
-=======
     // Close the files
     destinationFile.close();
->>>>>>> 0df8151 (hh)
 
 }
 
